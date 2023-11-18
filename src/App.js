45ce20df5c48b16d7fec2a9a852c9f377a7306b7
alswns6327 <alswns6327@naver.com>
2023-11-18 14:08:57 +0900

    todo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useReducer, useRef, useState } from "react";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 import TodoTemplate from "./components/TodoTemplate";
@@ -15,9 +15,21 @@ function createBulkTodos(){
   return array;
 }
 
+function todoReducer(todos, action){
+  switch(action.type){
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter(todo=> todo.id!==action.id);
+    case 'TOGGLE':
+      return todos.map(todo=> todo.id === action.id ? {...todo, checked: !todo.checked} : todo)
+  }
+}
+
 function App() {
 
-  const [todos, setTodos] = useState(createBulkTodos)
+  // const [todos, setTodos] = useState(createBulkTodos)
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos)
 
   const nextId = useRef(4501);
 
@@ -28,23 +40,23 @@ function App() {
         text,
         checked: false
       }
-      setTodos(todos => todos.concat(todo));
+      // setTodos(todos => todos.concat(todo));
+      dispatch({type : 'INSERT', todo});
     },
     [],
   )
 
   const onRemove = useCallback(
     id => {
-      setTodos(todos=>todos.filter(todo => todo.id != id));
+      // setTodos(todos=>todos.filter(todo => todo.id != id));
+      dispatch({type : 'REMOVE', id});
     },
     [],
   );
 
   const onToggle = useCallback(
     id => {
-      setTodos(todos=>
-        todos.map(todo => todo.id === id ? {...todo, checked: !todo.checked} : todo)
-      );
+      dispatch({type : 'TOGGLE', id});
     },
     [],
   );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import './TodoList.scss';
 import TodoListItem from './TodoListItem';
+import { List } from 'react-virtualized';
 
 const TodoList = ({todos, onRemove, onToggle}) => {
+
+    const rowRenderer = useCallback(
+        ({index, key, style}) => {
+            const todo = todos[index];
+            console.log(index);
+            console.log(key);
+            console.log(style);
+            return (
+                <TodoListItem
+                    todo = {todo}
+                    key={key}
+                    onRemove={onRemove}
+                    onToggle={onToggle}
+                    style={style}
+                />
+            )
+        },
+        [onToggle, onRemove, todos]
+    )
+
     return (
-        <div className='TodoList'>
-            {todos.map(todo=>(
-                <TodoListItem onToggle={onToggle} todo={todo} onRemove={onRemove} key={todo.id}/>
-            ))}
-        </div>
+        <List
+            className="TodoList"
+            width={512}
+            height={513}
+            rowCount={todos.length}
+            rowHeight={57}
+            rowRenderer={rowRenderer}
+            list={todos}
+            style={{outline: 'none'}}
+        />
     );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
